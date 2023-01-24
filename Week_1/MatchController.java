cfbe33bb42f8f83bb301ab9a436d4d0aebcedf0c
vvHacker007 <v6649@srmist.edu.in>
2023-01-24 12:22:17 +0530

    Code Refactoring

--- Week_1/MatchController.java
+++ Week_1/MatchController.java
@@ -4,29 +4,33 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MatchController {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main( String[] args ) throws InterruptedException {
         Team A = new Team("A");
         Team B = new Team("B");
         ArrayList<Team> teams = new ArrayList<>();
-        teams.add(A);
-        teams.add(B);
-        System.out.println("Let's have a toss");
-        int toss_winner = tossWin(teams);
-        System.out.println(teams.get(toss_winner).team_name+ " won the Toss and decides to bat");
-        while(teams.get(toss_winner).total_wickets!=0) {
-            teams.get(toss_winner).play();
+        teams.add( A );
+        teams.add( B );
+
+        System.out.println( "Let's have a toss" );
+        int toss_winner = tossWin( teams );
+        System.out.println( teams.get(toss_winner).team_name + " won the Toss and decides to bat" );
+
+        while( teams.get( toss_winner ).total_wickets != 0 ) {
+            teams.get( toss_winner ).play();
         }
-        System.out.println(teams.get(Math.abs(1-toss_winner)).team_name+ " needs a total of "+(teams.get(toss_winner).total_runs+1)+" runs to win the match.");
-        while(teams.get(Math.abs(1-toss_winner)).total_wickets!=0 && teams.get(Math.abs(1-toss_winner)).total_runs<=teams.get(toss_winner).total_runs) {
+        System.out.println( teams.get( Math.abs( 1-toss_winner )).team_name+ " needs a total of "+( teams.get( toss_winner ).total_runs + 1) + " runs to win the match." );
+        while( teams.get( Math.abs( 1 - toss_winner ) ).total_wickets != 0 && teams.get( Math.abs( 1 - toss_winner ) ).total_runs <= teams.get( toss_winner ).total_runs ) {
             teams.get(Math.abs(1-toss_winner)).play();
         }
-        if(A.total_wickets==B.total_wickets && A.total_runs==B.total_runs) main(args);
-        if(B.total_runs>A.total_runs) System.out.println("Team B Won the game by "+(B.total_runs-A.total_runs)+" runs and "+(B.total_wickets-A.total_wickets)+" wickets");
-        else System.out.println("Team A Won the game by "+(A.total_runs-B.total_runs)+" runs and "+(A.total_wickets-B.total_wickets)+" wickets");
+        
+        if( A.total_wickets == B.total_wickets && A.total_runs == B.total_runs) main( args );
+
+        if( B.total_runs > A.total_runs ) System.out.println( "Team B Won the game by " + ( B.total_runs - A.total_runs ) + " runs and " + ( B.total_wickets - A.total_wickets ) + " wickets" );
+        else System.out.println( "Team A Won the game by " + ( A.total_runs - B.total_runs ) + " runs and " + ( A.total_wickets - B.total_wickets ) + " wickets" );
     }
 
-    public static int tossWin(ArrayList<Team> teams) {
+    public static int tossWin( ArrayList<Team> teams ) {
         Random rand = new Random();
-        return rand.nextInt(teams.size());
+        return rand.nextInt( teams.size() );
     }
 }
--- Week_1/Team.java
+++ Week_1/Team.java
@@ -7,43 +7,43 @@ import static java.lang.Thread.sleep;
 
 public class Team {
     ArrayList<String> runs = new ArrayList<>();
-
-    public int total_runs=0;
-    public int total_wickets=10;
+    public int total_runs = 0;
+    public int total_wickets = 10;
     public String team_name = "";
-    public Team(String team_name) {
-        this.runs.add("0");
-        this.runs.add("1");
-        this.runs.add("W");
-        this.runs.add("5");
-        this.runs.add("6");
-        this.runs.add("W");
-        this.runs.add("2");
-        this.runs.add("3");
-        this.runs.add("W");
-        this.runs.add("4");
-        this.runs.add("W");
-        this.team_name=team_name;
+
+    public Team( String team_name ) {
+        this.runs.add( "0" );
+        this.runs.add( "1" );
+        this.runs.add( "W" );
+        this.runs.add( "5" );
+        this.runs.add( "6" );
+        this.runs.add( "W" );
+        this.runs.add( "2" );
+        this.runs.add( "3" );
+        this.runs.add( "W" );
+        this.runs.add( "4" );
+        this.runs.add( "W" );
+        this.team_name = team_name;
     }
 
     public void play() throws InterruptedException {
         Random rand = new Random();
-        String run = this.runs.get(rand.nextInt(this.runs.size()));
-        if(run.equals("W")) {
+        String run = this.runs.get( rand.nextInt( this.runs.size() ) );
+        if( run.equals( "W" ) ) {
             countWickets();
         } else {
-            addRuns(run);
+            addRuns( run );
         }
-        sleep(1000);
+        sleep( 1000 );
     }
 
-    public void addRuns(String run) {
-        this.total_runs += Integer.valueOf(run);
-        System.out.println("Team "+this.team_name+" scored a "+run+" which makes it a total of "+this.total_runs);
+    public void addRuns( String run ) {
+        this.total_runs += Integer.valueOf( run );
+        System.out.println( "Team " + this.team_name + " scored a " + run + " which makes it a total of " + this.total_runs );
     }
 
     public void countWickets() {
         this.total_wickets--;
-        System.out.println("Wickets were hit!!\nTotal of "+this.total_wickets+" wickets are remaining and Team "+this.team_name+" has scored a total of "+this.total_runs+" runs");
+        System.out.println( "Wickets were hit!!\nTotal of " + this.total_wickets + " wickets are remaining and Team " + this.team_name + " has scored a total of " + this.total_runs + " runs" );
     }
 }
